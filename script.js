d44fcb9aa1e69cdd075e9ae6a80abbf1d39f545f
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-30 12:34:26 +0200

    part1

--- script.js
+++ script.js
@@ -18,7 +18,49 @@ function cleanJson(text){
 
 function createCarouselCards(result) {
     const parsedResult = cleanJson(result); 
-    console.log(parsedResult);
+    const items = parsedResult.items; 
+
+    const template =   //   creo classe centered per centrare immagini, per cui dedico styel.css
+        ` 
+            <img src="#URL" class="centered d-block w-100" alt="#ALT"> 
+            <div class="carousel-caption d-none d-md-block div-background">
+              <h5>#TITLE</h5>
+            </div>
+        ` 
+    const container = document.getElementById('carousel-container'); 
+
+    container.innerHTML = '';
+    // console.log(container);
+    for (let i = 0; i  < items.length; i++) { 
+        
+        const item = items[i]; 
+
+        const div = document.createElement('div'); 
+        div.classList.add('carousel-item'); 
+        if (i === 0) { // al primo item abbiamo aggiunto la classe active, così che bootstrap possa operarci
+            div.classList.add('active');
+        }
+        const internalHtml = template.replace('#URL', item.media.m) 
+                                     .replace('#ALT', item.tags) 
+                                     .replace('#TITLE', item.title); 
+
+        div.innerHTML = internalHtml; 
+        
+        container.appendChild(div); 
+    } 
+    // console.log(container);
+} 
+
+function search() {
+    const input = document.getElementById('search-input'); 
+    const searchWords = input.value.trim(); 
+    const tags = searchWords.replaceAll(' ', ','); 
+    const tagsUrl = BASE_URL + '&tags=' + tags; 
+
+    fetch(tagsUrl) 
+    .then(response => response.text()) 
+    .then(result => createCarouselCards(result)) 
+    .catch(error => console.log(error))
 }
 
 initFlickr(); 
